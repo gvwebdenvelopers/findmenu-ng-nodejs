2ef5cd2718d31f0eb86420f291222f573c9898d9
Oscar Otero <oscar.otero.millan@gmail.com>
2017-02-14 05:56:39 +0100

    Archivos less

--- gulp.config.js
+++ gulp.config.js
@@ -43,7 +43,7 @@ module.exports = function() {
       '**/*.module.js',
       '**/*.js'
     ],
-    less: client + 'styles/styles.less',
+    less: client + 'styles/general.less',
     report: report,
     root: root,
     server: server,

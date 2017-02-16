2abfa81140f30725d2ce163048fb4ffbd465a243
Jorge <jordimart83@gmail.com>
2017-02-16 01:26:39 +0100

    Hago script y configuracion para hook

---
+++ hook/hook.js
@@ -0,0 +1,16 @@
+var gith = require('gith').create(6000);  // run on port 6000
+var exec = require('child_process').exec; 
+
+gith({
+  repo: 'gvwebdenvelopers/findmenu-ng-nodejs'  // the github-user/repo-name
+}).on('all', function(payload){
+
+    console.log("push received");
+    exec('/home/jordimart/findmenu-ng-nodejs/hook/hook.sh ' + payload.branch, function(err, stdout, stderr){
+      if (err) return err;
+      console.log(stdout);
+      console.log("git deployed to branch " + payload.branch);
+    });
+});
+
+

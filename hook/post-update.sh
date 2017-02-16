3564f4a1829ce5f59115c2fac7a02445f722d947
Jorge <jordimart83@gmail.com>
2017-02-16 03:05:00 +0100

    Prova para hook

--- hook/post-update.sh
+++ hook/post-update.sh
@@ -5,3 +5,4 @@ forever stop src/server/app.js
 git fetch origin && git pull origin master && bower install && npm install && gulp build && PORT=3000 NODE_ENV=build
 forever start ./src/server/app.js
 exec git update-server-info
+

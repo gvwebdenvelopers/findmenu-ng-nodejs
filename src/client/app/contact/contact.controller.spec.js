61178c106220ac9bcc92cf7c2d31b672d9e4d6ff
Jorge <jordimart83@gmail.com>
2017-02-23 21:22:18 +0100

    controlador contact testeado

--- src/client/app/contact/contact.controller.spec.js
+++ src/client/app/contact/contact.controller.spec.js
@@ -1,51 +1,92 @@
 /* jshint -W117, -W030 */
 describe('contact.controller', function() {
-var controller;
+    var controller;
 
-beforeEach(function() {
-  bard.appModule('app.contact');
-  bard.inject('$controller', '$log', '$q', '$rootScope');
-});
+    var emailFake;
 
-beforeEach(function() {
-  controller = $controller('ContactController');
-  $rootScope.$apply();
-});
+    beforeEach(function() {
+        bard.appModule('app.contact');
+        bard.inject('$controller', '$log', '$q', '$rootScope');
 
-bard.verifyNoOutstandingHttpRequests();
+        dsFake = {
+            sendEmail: function(data) {
+                return $q.when(true);
+            }
+        };
+        controller = $controller('ContactController', {
+            dataservice: dsFake
+        });
 
-describe('Contact controller', function() {
-  it('should be created successfully', function() {
-    expect(controller).to.be.defined;
-  });
-  it('should initially have a user with all fields empty', function() {
-    expect(controller.inputName).to.equal('');
-    expect(controller.inputEmail).to.equal('');
-    expect(controller.inputSubject).to.equal('');
-    expect(controller.inputMessage).to.equal('');
-  });
+    });
 
-  it('should initially functions are defined', function() {
-    expect(controller.SubmitContact).to.be.defined;
+    //bard.verifyNoOutstandingHttpRequests();
 
-  });
+    describe('Contact controller', function() {
 
-  /*describe('SubmitContact function', function() {
+        it('should be created successfully', function() {
+            expect(controller).to.be.defined;
+        });
+        it('should initially have a user with all fields empty', function() {
+            expect(controller.inputName).to.equal('');
+            expect(controller.inputEmail).to.equal('');
+            expect(controller.inputSubject).to.equal('');
+            expect(controller.inputMessage).to.equal('');
+        });
 
-      it('should be send two emails successfully', function () {
+        it('should initially functions are defined', function() {
+            expect(controller.SubmitContact).to.be.defined;
 
-          controller.inputName = 'jorge';
-          controller.inputEmail = 'jordimart83@gmail.com';
-          controller.inputSubject = 'mocktestemail';
-          controller.inputMessage = 'mocktestemail';
+        });
 
-      controller.SubmitContact();
-      $scope.$apply();
-      expect(controller.resultMessageOk).to.equal('Su email ha sido enviado correctamente');
+        describe('SubmitContact function', function() {
 
-  });*/
+            it('should be send two emails successfully', function() {
 
-});
+                controller.SubmitContact();
+                $rootScope.$apply();
+                expect(controller.resultMessageOk).to.equal('Su email ha sido enviado correctamente');
 
-});
+            });
+
+            it('should be send two emails not successfully', function() {
+
+                dsFake.sendEmail = function() {
+                    return $q.when(false);
+                };
+
+                var controller2 = $controller('ContactController', {
+                    dataservice: dsFake
+                });
+
+                controller.SubmitContact();
+                $rootScope.$apply();
+                expect(controller.resultMessageFail).to.equal('Ha habido un error al enviar ' +
+                    'el email, intentelo mas tarde');
+
+            });
+
+            it('should be send user email not successfully', function() {
+
+                dsFake.sendEmail = function(data) {
+                    if (data.type === 'admin') {
+                        return $q.when(true);
+                    } else {
+                        return $q.when(false);
+                    }
+                };
+
+                var controller2 = $controller('ContactController', {
+                    dataservice: dsFake
+                });
+
+                controller.SubmitContact();
+                $rootScope.$apply();
+                expect(controller.resultMessageFail).to.equal('Ha habido un error al enviar ' +
+                    'el email de usuario, intentelo mas tarde');
+
+            });
+
+        });
+
+    });
 });

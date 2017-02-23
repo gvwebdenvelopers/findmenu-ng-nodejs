df13e826a2b5f1d720dadbe1cdf6d8e3c2d86f6a
Oscar Otero <oscar.otero.millan@gmail.com>
2017-02-23 01:53:19 +0100

    Implementado mock de menus, test de rutas y estado de modulo menus

---
+++ src/client/app/menus/menus.route.spec.js
@@ -0,0 +1,31 @@
+/* jshint -W117, -W030 */
+describe('menus routes', function() {
+  describe('state menus', function() {
+    var view = 'app/menus/menus.html';
+
+    beforeEach(function() {
+      module('app.menus', bard.fakeToastr);
+      bard.inject('$httpBackend', '$rootScope', '$state', '$templateCache');
+    });
+
+    beforeEach(function() {
+      $templateCache.put(view, '');
+    });
+
+    bard.verifyNoOutstandingHttpRequests();
+
+    it('should map state menus to url /menus ', function() {
+      expect($state.href('menus', {})).to.equal('/menus');
+    });
+
+    it('should map /menus route to menus View template', function() {
+      expect($state.get('menus').templateUrl).to.equal(view);
+    });
+
+    it('of contact should work with $state.go', function() {
+      $state.go('menus');
+      $rootScope.$apply();
+      expect($state.is('menus'));
+    });
+  });
+});
--- src/client/test-helpers/mock-data.js
+++ src/client/test-helpers/mock-data.js
@@ -2,7 +2,8 @@
 var mockData = (function() {
   return {
     getMockPeople: getMockPeople,
-    getMockStates: getMockStates
+    getMockStates: getMockStates,
+    getMockMenus: getMockMenus,
   };
 
   function getMockStates() {
@@ -33,4 +34,15 @@ var mockData = (function() {
       { firstName: 'Haley', lastName: 'Guthrie', age: 35, location: 'Wyoming' }
     ];
   }
+
+  function getMockStates() {
+    return [
+      { id: 00, nombre: 'Elbar0', precio_menu: 13, valoracion: 4, latitud: 0.00000, longitud: 0.00000, direccion:'C/la direció' },
+      { id: 01, nombre: 'Elbar1', precio_menu: 12, valoracion: 3, latitud: 1.00000, longitud: 1.00000, direccion:'C/la direció' },
+      { id: 02, nombre: 'Elbar2', precio_menu: 11, valoracion: 1, latitud: 2.00000, longitud: 2.00000, direccion:'C/la direció' },
+      { id: 03, nombre: 'Elbar3', precio_menu: 10, valoracion: 0, latitud: 3.00000, longitud: 3.00000, direccion:'C/la direció' },
+      { id: 04, nombre: 'Elbar4', precio_menu: 9, valoracion: 4, latitud: 4.00000, longitud: 4.00000, direccion:'C/la direció' }
+    ];
+  }
+  
 })();

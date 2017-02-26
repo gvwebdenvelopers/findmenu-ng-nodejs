4214f2a24260b64deb5ccb679555b28d89710c52
Jorge <jordimart83@gmail.com>
2017-02-27 00:45:15 +0100

    elimino carpeta hook que no debe descargarse

--- hook/post-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-unset 'GIT_DIR'
-cd /home/jordimart/findmenu-ng-nodejs
-sudo forever stop ./src/server/app.js
-git fetch origin && git pull origin master && bower install && npm install && gulp build && sudo PORT=3000 NODE_ENV=build forever start ./src/server/app.js
-exec git update-server-info
-
